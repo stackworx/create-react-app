9755242ec431f0db520f9575c919f0cd8269524a
ciaranl <ciaran@stackworx.io>
2016-11-22 21:55:12 +0200

    Added relay

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -150,6 +150,7 @@ module.exports = {
         query: {
           babelrc: false,
           presets: [require.resolve('babel-preset-react-app')],
+          plugins: [require.resolve('../scripts/plugins/babelRelayPlugin')],
         },
         // @remove-on-eject-end
       },
